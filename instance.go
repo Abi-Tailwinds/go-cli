7b3fb96f0d3becf9f17808019418a2ed59237c00
abinaya <abinaya@tailwinds.ai>
2022-11-13 16:54:11 +0530

    bugs

--- instance.go
+++ instance.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bytes"
-	"encoding/json"
 	"fmt"
 	"log"
 	"os/exec"
@@ -11,16 +10,16 @@ import (
 )
 
 func vmcreate() (string, string) {
-	cmd1 := exec.Command("bash","-c","pulumi login --local")
-        log.Printf("%v",cmd1)
-        var stderr bytes.Buffer
-        cmd1.Stderr = &stderr
-        out1, err1 := cmd1.Output()
-        if err1 != nil {
-                log.Printf("STDERR of pulumi login command : %v\n", stderr.String())
-        }
-        log.Printf("Output of pulumi login command : %v\n", string(out1))
-		cmd5 := exec.Command("pulumi","stack","init","dev1","--cwd","/home/Abi-dev/pulumi/create-delete-aws-vm")
+	cmd1 := exec.Command("bash", "-c", "pulumi login --local")
+	log.Printf("%v", cmd1)
+	var stderr bytes.Buffer
+	cmd1.Stderr = &stderr
+	out1, err1 := cmd1.Output()
+	if err1 != nil {
+		log.Printf("STDERR of pulumi login command : %v\n", stderr.String())
+	}
+	log.Printf("Output of pulumi login command : %v\n", string(out1))
+	cmd5 := exec.Command("pulumi", "stack", "init", "dev1", "--cwd", "/home/Abi-dev/pulumi/create-delete-aws-vm")
 	log.Printf("%v", cmd5)
 	cmd5.Stderr = &stderr
 	out5, err5 := cmd5.Output()
@@ -28,31 +27,31 @@ func vmcreate() (string, string) {
 		log.Printf("STEDERR OF pulumi stack init command: %v/n", stderr.String())
 	}
 	log.Printf("%v", string(out5))
-	cmd2 := exec.Command("pulumi","stack","select","dev1","--cwd","/home/Abi-dev/pulumi/create-delete-aws-vm")
-        log.Printf("%v",cmd2)
-        //var stderr bytes.Buffer
-        cmd2.Stderr = &stderr
-        out2, err2 := cmd2.Output()
-        if err2 != nil {
-                log.Printf("STDERR of pulumi stack select command : %v\n", stderr.String())
-        }
-        log.Printf("Output of pulumi stack select command : %v\n", string(out2))
-		cmd6 := exec.Command("pulumi","plugin","install","resource","aws","5.13.0","--cwd","/home/Abi-dev/pulumi/create-delete-aws-vm")
-        log.Printf("%v",cmd6)
-        cmd6.Stderr = &stderr
-        out6, err6 := cmd6.Output()
-        if err6 != nil {
-                log.Printf("STDERR of pulumi plugin  select command : %v\n", stderr.String())
-        }
-        log.Printf("Output of pulumi plugin  select command : %v\n", string(out6))
-	cmd7 := exec.Command("pulumi","config","set","aws:region","us-east-1","--cwd","/home/Abi-dev/pulumi/create-delete-aws-vm")
-        log.Printf("%v",cmd7)
-        cmd6.Stderr = &stderr
-        out7, err7 := cmd7.Output()
-        if err7 != nil {
-                log.Printf("STDERR of pulumi plugin  select command : %v\n", stderr.String())
-        }
-        log.Printf("Output of pulumi plugin  select command : %v\n", string(out7)) 
+	cmd2 := exec.Command("pulumi", "stack", "select", "dev1", "--cwd", "/home/Abi-dev/pulumi/create-delete-aws-vm")
+	log.Printf("%v", cmd2)
+	//var stderr bytes.Buffer
+	cmd2.Stderr = &stderr
+	out2, err2 := cmd2.Output()
+	if err2 != nil {
+		log.Printf("STDERR of pulumi stack select command : %v\n", stderr.String())
+	}
+	log.Printf("Output of pulumi stack select command : %v\n", string(out2))
+	cmd6 := exec.Command("pulumi", "plugin", "install", "resource", "aws", "5.13.0", "--cwd", "/home/Abi-dev/pulumi/create-delete-aws-vm")
+	log.Printf("%v", cmd6)
+	cmd6.Stderr = &stderr
+	out6, err6 := cmd6.Output()
+	if err6 != nil {
+		log.Printf("STDERR of pulumi plugin  select command : %v\n", stderr.String())
+	}
+	log.Printf("Output of pulumi plugin  select command : %v\n", string(out6))
+	cmd7 := exec.Command("pulumi", "config", "set", "aws:region", "us-east-1", "--cwd", "/home/Abi-dev/pulumi/create-delete-aws-vm")
+	log.Printf("%v", cmd7)
+	cmd6.Stderr = &stderr
+	out7, err7 := cmd7.Output()
+	if err7 != nil {
+		log.Printf("STDERR of pulumi plugin  select command : %v\n", stderr.String())
+	}
+	log.Printf("Output of pulumi plugin  select command : %v\n", string(out7))
 	cmd := exec.Command("pulumi", "up", "--cwd", "/home/Abi-dev/pulumi/create-delete-aws-vm", "--config=vmname=awsclivm", "--config=subnetid=subnet-9cf64cbd", "--config=vpcid=vpc-9df038e0", "--config=keyname=awskey4", "--non-interactive", "-y")
 	fmt.Println(cmd)
 	cmd.Stderr = &stderr
@@ -79,8 +78,8 @@ func vmcreate() (string, string) {
 			log.Printf("%s", IP)
 		}
 		ID = v[44]
-			log.Printf("%s", v[44])
-			log.Printf("%s", ID)
+		log.Printf("%s", v[44])
+		log.Printf("%s", ID)
 
 	}
 	if IP[0] == '"' {
@@ -111,9 +110,6 @@ func main() {
 		log.Printf("STDERR of keypair command : %v\n", stderr.String())
 	}
 	log.Printf("Output of keypair command : %v\n", string(out))
-	var m map[string]interface{}
-	err = json.Unmarshal(out, &m)
-	//	result := m["InstanceStatuses"].(map[string]interface{})["SystemStatus"]
 	result := string(out)
 	fmt.Println(result)
 	var str1 = "Status"
@@ -121,15 +117,18 @@ func main() {
 	v := strings.Fields(result)
 	fmt.Println(v)
 	for i := 0; i < len(v); i++ {
+		log.Println(i, v[i])
 		match1, err1 := regexp.MatchString(str1, v[i])
 		if err1 != nil {
 			log.Println(err)
 		}
 		if match1 {
-			IP = v[i]
+			IP = v[i+1]
 			if IP == "passed" {
 				log.Println("passed")
 				break
+			} else {
+				log.Println("no match")
 			}
 		}
 	}
